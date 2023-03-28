dc27964da6528f9aa81c283348610c1ce415fceb
Angelisx <bladetax132@gmail.com>
2023-03-28 00:40:11 -0400

    added working click function on planet

--- main.js
+++ main.js
@@ -4,12 +4,11 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import Stats from "three/addons/libs/stats.module.js";
+import { TWEEN } from 'three/examples/jsm/libs/tween.module.min'
 
-let container, stats;
-let camera, scene, raycaster, renderer;
 
-let INTERSECTED;
-let theta = 0;
+let container;
+let camera, scene, raycaster, renderer,delta;
 
 const pointer = new THREE.Vector2();
 const radius = 100;
@@ -17,6 +16,7 @@ const radius = 100;
 init();
 function init() {
   const clock = new THREE.Clock();
+
   // Set up camera and scene
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(
@@ -30,142 +30,102 @@ function init() {
   });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
-  camera.position.setZ(9);
-  camera.position.setX(0);
-  camera.position.setY(5);
+  camera.position.set(0, 5, 9);
   camera.lookAt(scene.position);
+
+
+  const spaceTexture = new THREE.TextureLoader().load("space.jpg");
+  scene.background = spaceTexture;
 
   container = document.createElement("div");
   document.body.appendChild(container);
-
-
- 
-  raycaster = new THREE.Raycaster();
-
-  renderer = new THREE.WebGLRenderer();
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
   container.appendChild(renderer.domElement);
-
-  stats = new Stats();
-  container.appendChild(stats.dom);
-
-  document.addEventListener("mousemove", onPointerMove);
-
-  //
-
   window.addEventListener("resize", onWindowResize);
 }
 
-function onPointerMove(event) {
-  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-  pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
-}
+function planetclicker() {
+  // assuming you have a planet mesh called 'planet'
+
+  // create a raycaster object to detect clicks on the planet
+  const raycaster = new THREE.Raycaster();
+
+  // add event listener for mouse clicks
+  renderer.domElement.addEventListener("click", onMouseClick, false);
+
+  function onMouseClick(event) {
+    // calculate mouse position in normalized device coordinates
+    const mouse = {
+      x: (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
+      y: -(event.clientY / renderer.domElement.clientHeight) * 2 + 1,
+    }
+    // update the picking ray with the camera and mouse position
+    raycaster.setFromCamera(mouse, camera);
+
+    // calculate objects intersecting the picking ray
+    const intersects = raycaster.intersectObjects([planet]);
 
 
-
-function render() {
-
-  theta += 0.1;
-
-  camera.lookAt( scene.position );
-
-  camera.updateMatrixWorld();
-
-  // find intersections
-
-  raycaster.setFromCamera( pointer, camera );
-
-  const intersects = raycaster.intersectObjects( scene.children, false );
-
-  if ( intersects.length > 0 ) {
-
-    if ( INTERSECTED != intersects[ 0 ].object ) {
-
-      if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-
-      INTERSECTED = intersects[ 0 ].object;
-      INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-      INTERSECTED.material.emissive.setHex( 0xff0000 );
+    if (intersects.length > 0) {
+      // user clicked on the planet, animate camera to move towards the planet
+      console.log('it clicks')
+      window.location.href = 'planet.html';
 
     }
-
-  } else {
-
-    if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-
-    INTERSECTED = null;
-
   }
-
-
-
 }
 
 
-const spheregeometry = new THREE.SphereGeometry( 0.5, 32, 16 );
-const spherematerial = new THREE.MeshStandardMaterial( { color: 0xae15d4} );
-const sphere = new THREE.Mesh( spheregeometry, spherematerial );
-sphere.position.set(3.8,4.3,0);
-scene.add( sphere )
 
 
 
 
 
 
-
-
-
-
-
-
-
-
+const geometry = new THREE.SphereGeometry(0.5, 32, 16);
+const material = new THREE.MeshStandardMaterial({ color: 0xae15d4 });
+const planet = new THREE.Mesh(geometry,material);
+planet.position.set(3.8, 4.3, 0);
+scene.add(planet);
 
 //spaceTexture
-const spaceTexture = new THREE.TextureLoader().load("space.jpg");
-scene.background = spaceTexture;
+
 
 // orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enabled = true;
 controls.maxPolarAngle = Math.PI / 2;
 
-//square
-// const geometry = new THREE.BoxGeometry(3, 3, 3);
-// const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-// const cube = new THREE.Mesh(geometry, material);
-// cube.position.y = 1.5;
-// scene.add(cube);
-
 
 
 //light source
+const pointlight = new THREE.PointLight(0xffffff);
+pointlight.position.set(5, 5, 5);
+const light = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(pointlight);
 
-  const pointlight = new THREE.PointLight(0xffffff);
-  pointlight.position.set(5, 5, 5);
-  const light = new THREE.AmbientLight( 0x404040 ); // soft white light
-  scene.add( pointlight );
+//3d model
+const loader = new GLTFLoader();
+loader.load(
+  "assets/space_boi/scene.gltf",
+  function (gltf) {
+    scene.add(gltf.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
 
-function model() {
-  const loader = new GLTFLoader();
 
-  loader.load(
-    "assets/space_boi/scene.gltf",
-    function (gltf) {
-      scene.add(gltf.scene);
-    },
-    undefined,
-    function (error) {
-      console.error(error);
-    }
-  );
-}
+
 // //helpers
-// const lighthelper = new THREE.PointLightHelper(pointlight);
-// const gridhelper = new THREE.GridHelper(200, 50);
-// scene.add(lighthelper, gridhelper);
+;
+
+function helpers() {
+  const lighthelper = new THREE.PointLightHelper(pointlight);
+  const gridhelper = new THREE.GridHelper(200, 50);
+  scene.add(lighthelper, gridhelper);
+}
 
 // //window resize
 function onWindowResize() {
@@ -177,7 +137,7 @@ function onWindowResize() {
 function Cameraspin() {
   controls.update();
 
-  controls.autoRotate = false;
+  controls.autoRotate = true;
   controls.autoRotateSpeed = -2;
 }
 function animate() {
@@ -185,11 +145,9 @@ function animate() {
   // group.rotation += 0.005;
   setTimeout(Cameraspin, 1000);
   renderer.render(scene, camera);
-  stats.update();
-  model();
-  render();
-  Light();
+  planetclicker();
 
 }
 
+helpers()
 animate();
