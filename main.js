b5dedf864b089e4f77ba89b43df62bcdfe21efa1
Angelisx <bladetax132@gmail.com>
2023-03-30 05:27:09 -0400

    summary: I removed the old files and added the new ones, I also added the new audio files and the new font files. I also added the new images and the new models. I also added the new script.js file

--- main.js
+++ main.js
@@ -1,23 +1,49 @@
 import "./style.css";
+import { fade } from "./script.js";
+
 
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import Stats from "three/addons/libs/stats.module.js";
-import * as TWEEN from '@tweenjs/tween.js';
+import * as TWEEN from "@tweenjs/tween.js";
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+
 
 
 let container;
-let camera, scene, raycaster, renderer,delta;
+let camera, scene, raycaster, renderer, delta;
 
-const pointer = new THREE.Vector2();
-const radius = 100;
 let updateControls = true;
+
+//3d model
+const loader = new GLTFLoader();
+loader.load(
+  "assets/space_boi/test models/something/spaceman3.gltf",
+  function (gltf) {
+    const spaceMan = gltf.scene;
+    scene.add(spaceMan);
+
+
+    spaceMan.traverse(function(child) {
+      if (child.isMesh) {
+        child.material.opacity = 1;
+        child.material.transparent = true;
+        }
+    });
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
 
 init();
 function init() {
   const clock = new THREE.Clock();
-
 
   // Set up camera and scene
   scene = new THREE.Scene();
@@ -30,24 +56,22 @@ function init() {
   renderer = new THREE.WebGL1Renderer({
     canvas: document.querySelector("#bg"),
   });
+
+
+
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
-  camera.position.set(0, 5, 9);
+  camera.position.set(0, 8, 12);
   camera.lookAt(scene.position);
   // camera.near = 0.1; // the nearest distance to render
   // camera.far = 30; // the farthest distance to render
   camera.updateProjectionMatrix();
 
-
-
-
   //space background
-  const spaceTexture = new THREE.TextureLoader().load("space.jpg");
+  const spaceTexture = new THREE.TextureLoader().load("assets/images/8k_stars.jpg");
   scene.background = spaceTexture;
 
-
   window.addEventListener("resize", onWindowResize);
-
 }
 
 function planetclicker() {
@@ -64,57 +88,100 @@ function planetclicker() {
     const mouse = {
       x: (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
       y: -(event.clientY / renderer.domElement.clientHeight) * 2 + 1,
-    }
+    };
     // update the picking ray with the camera and mouse position
     raycaster.setFromCamera(mouse, camera);
 
     // calculate objects intersecting the picking ray
     const intersectsProjects = raycaster.intersectObjects([projectsPlanet]);
     const intersectsContact = raycaster.intersectObjects([contactPlanet]);
-
+    const intersectsAvatar = raycaster.intersectObjects([AvatarBox]);
 
     if (intersectsProjects.length > 0) {
       // user clicked on the planet, animate camera to move towards the planet
-      console.log('it clicks')
-      
+      console.log("it clicks");
       updateControls = false;
 
-      const targetPosition = projectsPlanet.position
-      const startPosition = camera.position
-      const tween = new TWEEN.Tween(startPosition)
-      .to(targetPosition, 2000)
-      .easing(TWEEN.Easing.Quadratic.Out)
-      .onUpdate(() => {
-        camera.position.copy(startPosition);
-        camera.lookAt(projectsPlanet.position);
-      })
-      .onComplete(() => {
-        // transition to different page after camera animation is complete
 
-        camera.position.set(0, 5, 9);
-      })
-      .start();
+      const targetPosition = projectsPlanet.position;
+      const startPosition = camera.position;
+      const tween = new TWEEN.Tween(startPosition)
+        .to(targetPosition, 2000)
+        .easing(TWEEN.Easing.Quadratic.Out)
+        .onUpdate(() => {
+          camera.position.copy(startPosition);
+          camera.lookAt(projectsPlanet.position);
+        })
+        .onComplete(() => {
+          // transition to different page after camera animation is complete
+
+          camera.position.set(30, 5, 9);
+          controls.target.copy(camera.position);
+          camera.position.set(30, 5, 14);
+          updateControls = true;
+
+          fade();
+        })
+        .start();
     }
+
     if (intersectsContact.length > 0) {
       // user clicked on the planet, animate camera to move towards the planet
-      console.log('it clicks')
-      
+      console.log("it clicks");
+
       updateControls = false;
 
-      const targetPosition = contactPlanet.position
-      const startPosition = camera.position
+      const targetPosition = contactPlanet.position;
+      const startPosition = camera.position;
       const tween = new TWEEN.Tween(startPosition)
-      .to(targetPosition, 2000)
-      .easing(TWEEN.Easing.Quadratic.Out)
-      .onUpdate(() => {
-        camera.position.copy(startPosition);
-        camera.lookAt(contactPlanet.position);
-      })
-      .onComplete(() => {
-        // transition to different page after camera animation is complete
-        window.location.href = 'ContactPlanet.html';
-      })
-      .start();
+        .to(targetPosition, 2000)
+        .easing(TWEEN.Easing.Quadratic.Out)
+        .onUpdate(() => {
+          camera.position.copy(startPosition);
+          camera.lookAt(contactPlanet.position);
+        })
+        .onComplete(() => {
+          // transition to different page after camera animation is complete
+          camera.position.set(30, 5, 9);
+          fade();
+
+        })
+        .start();
+    }
+    if (intersectsAvatar.length > 0) {
+      // user clicked on the planet, animate camera to move towards the planet
+      
+      let audio = new Audio("assets/sound/HeartBeat_Loop_120bpm.mp3"); 
+      audio.volume = 0.01;
+      audio.play();
+      
+      audio.addEventListener('loadedmetadata', function() {
+
+      console.log(audio.duration);
+    });
+      console.log("it clicks");
+      updateControls = false;
+     
+      const targetPosition = AvatarBox.position;
+      const startPosition = camera.position;
+      const tween = new TWEEN.Tween(startPosition)
+        .to(targetPosition, 2000)
+        .easing(TWEEN.Easing.Quadratic.Out)
+        .onUpdate(() => {
+          camera.position.copy(startPosition);
+          camera.lookAt(AvatarBox.position);
+        })
+        .onComplete(() => {
+          // transition to different page after camera animation is complete
+
+          camera.position.set(30, 5, 9);
+          controls.target.copy(camera.position);
+          camera.position.set(30, 5, 14);
+          updateControls = true;
+
+          fade();
+        })
+        .start();
     }
   }
 }
@@ -122,21 +189,55 @@ function planetclicker() {
 
 
 
+function addStar() {
+  const stargeometry = new THREE.SphereGeometry(0.025, 24, 24);
+  const starmaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(stargeometry, starmaterial);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+Array(1000).fill().forEach(addStar);
+
+
 
 //objects
-const transparentmaterial = new THREE.MeshPhongMaterial({ color: 0x000000 ,transparent: true, opacity: 0,});
-const geometry = new THREE.SphereGeometry(0.5, 32, 16, true);
-const material = new THREE.MeshPhongMaterial({ color: 0x000000 ,transparent: true, opacity: 1,  side: THREE.DoubleSide});
-const projectsPlanet = new THREE.Mesh(geometry,material);
-const contactPlanet = new THREE.Mesh(geometry,transparentmaterial);
+const transparentmaterial = new THREE.MeshPhongMaterial({
+  color: 0xFFFFFF,
+  transparent: true,
+  opacity: 0,
+});
+const geometry = new THREE.SphereGeometry(0.4, 32, 16, true);
+const material = new THREE.MeshPhongMaterial({
+  color: 0x000000,
+  transparent: true,
+  opacity: 1,
+  side: THREE.DoubleSide,
+});
 
-projectsPlanet.position.set(3.8, 4.3, 0);
-contactPlanet.position.set(-3.6, 3.9, 0);
+const projectsPlanet = new THREE.Mesh(geometry, material);
+const contactPlanet = new THREE.Mesh(geometry, transparentmaterial);
 
-scene.add(contactPlanet,projectsPlanet);
+projectsPlanet.position.set(3.9, 4.3, 0);
+contactPlanet.position.set(-3.87, 4.55, -.84);
 
 
+const Avatargeometry = new THREE.BoxGeometry( 0.8, 2.5, .8);
+const Avatarmaterial = new THREE.MeshBasicMaterial( {color: 0x00ff00,
+  transparent: true,
+  opacity: 0} );
+const AvatarBox = new THREE.Mesh( Avatargeometry, Avatarmaterial );
+AvatarBox.position.set(0, 1, 0);
 
+const group = new THREE.Group();
+group.add( AvatarBox );
+group.add( projectsPlanet );
+group.add( contactPlanet );
+scene.add(group);
 
 
 
@@ -147,35 +248,18 @@ controls.enabled = true;
 controls.maxPolarAngle = Math.PI / 2;
 
 
-
 //light source
 const pointlight = new THREE.PointLight(0xffffff);
 pointlight.position.set(5, 5, 5);
 const light = new THREE.AmbientLight(0x404040); // soft white light
 scene.add(pointlight);
 
-//3d model
-const loader = new GLTFLoader();
-loader.load(
-  "assets/space_boi/scene.gltf",
-  function (gltf) {
-    scene.add(gltf.scene);
-  },
-  undefined,
-  function (error) {
-    console.error(error);
-  }
-);
-
-
-
 //helpers
 function helpers() {
-  const lighthelper = new THREE.PointLightHelper(pointlight);
-  const gridhelper = new THREE.GridHelper(200, 50);
-  scene.add(lighthelper, gridhelper);
+  // const lighthelper = new THREE.PointLightHelper(pointlight);
+  const gridHelper = new THREE.GridHelper(200, 50);
+  scene.add(gridHelper);
 }
-
 // //window resize
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -187,17 +271,21 @@ function Cameraspin() {
   if (updateControls) {
     controls.update();
   }
-  controls.autoRotate = false;
+  controls.autoRotate = true;
   controls.autoRotateSpeed = -2;
 }
 function animate() {
   requestAnimationFrame(animate);
-  // group.rotation += 0.005;
-  setTimeout(Cameraspin, 1000);
+  // setTimeout(Cameraspin, 2000);
+  // Cameraspin();
   renderer.render(scene, camera);
   planetclicker();
   TWEEN.update();
+  
 }
 
-helpers()
+
+
+
+// helpers();
 animate();
---
+++ script.js
@@ -0,0 +1,15 @@
+import "./style.css";
+
+function fade() {
+  var section1 = document.getElementById("section1");
+  var section2 = document.getElementById("section2");
+
+  // Fade out section 1
+  section1.classList.add("hidden");
+
+  // Fade in section 2 after a short delay
+  setTimeout(function () {
+    section2.classList.remove("hidden");
+  }, 1000);
+}
+export { fade };
