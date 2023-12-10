cad08291dd7638ddb75e9a2f16a6af9250c7fded
Angel Smythe <bladetax132@gmail.com>
2023-12-09 20:11:59 -0500

    angel mac

---
+++ test/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    const preloader = document.querySelector('.preloader');
+    preloader.classList.add('loaded'); // Add 'loaded' class when everything is loaded
+  });
+  
